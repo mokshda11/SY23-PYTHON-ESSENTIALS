0d263dbdbe3be36bd7f16c3b7c6b618ccb1b67a9
mokshda11 <mokshdamankar11@gmail.com>
2026-07-24 12:22:04 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,14 @@
+#Create a list of assignments
+assignment = ['math', 'chem', 'phy']
+print("The initial list of assignments is: ", assignment)
+
+#Add a new assignment to the list
+assignment.append ('bio')
+print("The new list of assignment is: ", assignment)
+
+#Remove an assignment
+assignment.remove ('math')
+print("The new list after removing one assignment is: ", assignment)
+
+#Print the updated list
+assignmet("The updated list of assignments is: ", assignment)
